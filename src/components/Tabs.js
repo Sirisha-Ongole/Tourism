b0ba0b0d00baff11408922ab3663bd7e39ac5c2a
Sirisha Ongole <ongole.sirisha@gmail.com>
2023-10-11 20:30:03 -0400

    Donations Page

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -10,12 +10,12 @@ export const Tabs = ({ data }) => {
 
   return (
     <div class="border-b border-gray-200 dark:border-gray-700  text-black">
-      <ul class="flex flex-wrap -mb-px text-sm font-medium text-center ">
+      <ul class="flex flex-wrap -mb-px text-xl font-medium text-center ">
         {
           // Map through the tabs data
           data.map((tab, index) => (
             <li
-              class={`px-1 pb-4 mr-1 cursor-pointer ${
+              class={`p-12 pb-4 mr-1 cursor-pointer ${
                 // if active tab, show underline with animation from left to right
                 activeTab === index
                   ? "border-b-2 border-black transition-all duration-500 transform"
